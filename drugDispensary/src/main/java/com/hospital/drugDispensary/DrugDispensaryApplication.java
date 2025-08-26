839bdbe999ba5ec72582e578e7bfdf110c667c42
Macwinner1 <ifechukwuw@gmail.com>
2025-08-26 10:18:51 +0100

    hospital project

---
+++ drugDispensary/src/main/java/com/hospital/drugDispensary/DrugDispensaryApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.drugDispensary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugDispensaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugDispensaryApplication.class, args);
+	}
+
+}
---
+++ drugDispensary/src/test/java/com/hospital/drugDispensary/DrugDispensaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.drugDispensary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugDispensaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
